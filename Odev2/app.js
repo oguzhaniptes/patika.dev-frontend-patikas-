7a9e27ebc248c43172c3b4224ed7c37a2c02982d
Oğuzhan İpteş <53427115+oguzhaniptes@users.noreply.github.com>
2023-03-06 23:06:19 +0300

    toast notification added

--- Odev2/app.js
+++ Odev2/app.js
@@ -2,6 +2,10 @@ let input = document.querySelector('#input')
 let addBtn = document.querySelector('#addBtn')
 let list = document.querySelector('#list')
 let checked = false;
+let tot = document.querySelector('.toast')
+let title = document.getElementById('toast-title')
+let message = document.getElementById('message')
+
 
 addBtn.addEventListener('click', Add)
 list.addEventListener('click', Control)
@@ -23,6 +27,13 @@ function Add(event) {
         newTodo.appendChild(deleteBtn)
         list.appendChild(tododiv)
         input.value = ""
+
+
+        message.innerHTML = "Ekleme başarılı şekilde gerçekleşti."
+        title.innerHTML = 'İşlem başarılı.'
+        var toast = new bootstrap.Toast(tot)
+        console.log(tot.classList)
+        toast.show()
     }
     else { alert("Hiç bir şey yapmayacaksın öyle mi ? Kabul edemiyoruz malesef bir görev gir :)") }
 
@@ -32,11 +43,23 @@ function Control(e) {
     const focusItem = e.target
     if (focusItem.classList[0] === "deleteBtn") {
         const item = focusItem.parentElement;
+        message.innerHTML = "Silme başarılı şekilde gerçekleşti."
+        title.innerHTML = 'İşlem başarılı.'
+        var toast = new bootstrap.Toast(tot)
+        toast.show()
         item.remove()
     }
     else if (focusItem.classList[0] === 'text') {
         const myLi = focusItem
         myLi.classList.toggle('checked')
+        if (myLi.classList.contains('checked')) {
+            message.innerHTML = "Görev başarılı şekilde tamamlandı."
+            title.innerHTML = 'İşlem başarılı.'
+        } else {
+            message.innerHTML = "Görev tamamlaması iptal edildi."
+            title.innerHTML = 'Görev silindi.'
+        }
+        var toast = new bootstrap.Toast(tot)
+        toast.show();
     }
 }
-
